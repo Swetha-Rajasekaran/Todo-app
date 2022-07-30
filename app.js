9a33309aae0ebf165acb78d38d80bca6978ee074
Swetha-Rajasekaran <swetha.rajasekaran10@gmail.com>
2022-07-30 20:06:38 +0530

    add: todo lists in the homepage

---
+++ app.js
@@ -0,0 +1,42 @@
+//selectors
+
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+
+function addTodo(event) {
+  event.preventDefault();
+  //todoDiv
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  //create Li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "Hello";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  //checkmark button
+
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  //trashmark button
+
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  //append to list
+
+  todoList.appendChild(todoDiv);
+}
